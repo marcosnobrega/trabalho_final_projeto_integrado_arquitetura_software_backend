c33c3ea9052bddcf93f425fb48c055f052b676b7
Marcos Nobrega <marcosnobregajr@gmail.com>
2022-08-23 20:04:35 -0300

    Updated project configuration

--- src/main/java/com/pucminas/piol/ollen/config/SecurityConfiguration.java
+++ src/main/java/com/pucminas/piol/ollen/config/SecurityConfiguration.java
@@ -58,7 +58,6 @@ public class SecurityConfiguration {
                 .antMatchers("/app/**/*.{js,html}")
                 .antMatchers("/i18n/**")
                 .antMatchers("/content/**")
-                .antMatchers("/h2-console/**")
                 .antMatchers("/swagger-ui/**")
                 .antMatchers("/test/**");
     }
